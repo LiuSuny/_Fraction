6b0c2c6c5285017bc6aa0f63426779d50edffd75
LiuSuny <119320935+LiuSuny@users.noreply.github.com>
2023-11-16 15:11:58 +0300

    Add project files.

---
+++ _Fraction/Fraction.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _Fraction
+{
+    class Fraction
+    {
+        private double numerator;
+        private double denominator;
+
+        public Fraction(double numerator, double denominator)
+        {
+            if (denominator == 0)
+            {
+                throw new ArgumentException(" Zero cannot be Denominator.");
+            }
+
+            this.numerator = numerator;
+            this.denominator = denominator;
+            Reduce();
+            Console.WriteLine($"2args Constructor :\t{GetHashCode()}");
+        }
+
+        private void Reduce()
+        {
+            double gcd = FindGCD(Math.Abs(numerator), Math.Abs(denominator));
+            numerator /= gcd;
+            denominator /= gcd;
+
+            if (denominator < 0)
+            {
+                numerator = -numerator;
+                denominator = -denominator;
+            }
+        }
+
+        private double FindGCD(double a, double b)
+        {
+            while (b != 0)
+            {
+                double temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+
+        public override string ToString()
+        {
+            if (denominator == 1)
+            {
+                return numerator.ToString();
+            }
+            return $"{numerator}/{denominator}";
+        }
+
+        public static Fraction operator +(Fraction fraction1, Fraction fraction2)
+        {
+            double resultNumerator = fraction1.numerator * fraction2.denominator + fraction2.numerator * fraction1.denominator;
+            double resultDenominator = fraction1.denominator * fraction2.denominator;
+            return new Fraction(resultNumerator, resultDenominator);
+        }
+
+        public static Fraction operator -(Fraction fraction1, Fraction fraction2)
+        {
+            double resultNumerator = fraction1.numerator * fraction2.denominator - fraction2.numerator * fraction1.denominator;
+            double resultDenominator = fraction1.denominator * fraction2.denominator;
+            return new Fraction(resultNumerator, resultDenominator);
+        }
+
+        public static Fraction operator *(Fraction fraction1, Fraction fraction2)
+        {
+            double resultNumerator = fraction1.numerator * fraction2.numerator;
+            double resultDenominator = fraction1.denominator * fraction2.denominator;
+            return new Fraction(resultNumerator, resultDenominator);
+        }
+
+        public static Fraction operator /(Fraction fraction1, Fraction fraction2)
+        {
+            if (fraction2.numerator == 0)
+            {
+                throw new DivideByZeroException("Division of zero not allow.");
+            }
+
+            double resultNumerator = fraction1.numerator * fraction2.denominator;
+            double resultDenominator = fraction1.denominator * fraction2.numerator;
+            return new Fraction(resultNumerator, resultDenominator);
+        }
+    }
+}
---
+++ _Fraction/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _Fraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Fraction f1 = new Fraction(4, 5);
+            //Fraction f2 = new Fraction(3, 7);
+            //Console.WriteLine(f1.subtraction(f2));
+
+
+            Fraction f1 = new Fraction(2, 3);
+            Fraction f2 = new Fraction(1, 2);
+            Fraction f3 = new Fraction(5, 1);
+
+            // Example of the operation
+            Fraction addition = f1 + f2;
+            Fraction subtraction = f1 - f2;
+            Fraction multiply = f1 * f2;
+            Fraction division = f1 / f2;
+
+            // Вывод результатов
+            Console.WriteLine($"Fraction1: {f1}");
+            Console.WriteLine($"Fraction2: {f2}");
+            Console.WriteLine($"Fraction3: {f3}");
+            Console.WriteLine($"Addition: {addition}");
+            Console.WriteLine($"Subtraction: {subtraction}");
+            Console.WriteLine($"Multiply: {multiply}");
+            Console.WriteLine($"Division: {division}");
+
+        }
+    }
+}
