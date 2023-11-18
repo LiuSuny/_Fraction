2440c98808dd6f6ff69fe63dcb5b91f57d760321
LiuSuny <119320935+LiuSuny@users.noreply.github.com>
2023-11-18 22:58:52 +0300

    Fraction updated DONE

--- _Fraction/Fraction.cs
+++ _Fraction/Fraction.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿#define CLASS_FRACTION
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,86 +9,196 @@ namespace _Fraction
 {
     class Fraction
     {
-        private double numerator;
-        private double denominator;
 
-        public Fraction(double numerator, double denominator)
+        int denominator;
+        public int Integer { get; set; }
+        public int Numerator { get; set; }
+
+        public int Denominator
         {
-            if (denominator == 0)
-            {
-                throw new ArgumentException(" Zero cannot be Denominator.");
-            }
-
-            this.numerator = numerator;
-            this.denominator = denominator;
-            Reduce();
-            Console.WriteLine($"2args Constructor :\t{GetHashCode()}");
+            get { return denominator; }
+            set { if (value < 0) value = 1; this.denominator = value; }
         }
 
-        private void Reduce()
+        public Fraction(int integer)
         {
-            double gcd = FindGCD(Math.Abs(numerator), Math.Abs(denominator));
-            numerator /= gcd;
-            denominator /= gcd;
-
-            if (denominator < 0)
-            {
-                numerator = -numerator;
-                denominator = -denominator;
-            }
+            Integer = integer;
+            Numerator = 0;
+            denominator = 1;
+            Console.WriteLine($"Constructor : \t{GetHashCode()}");
+        }
+        public Fraction(int numerator, int denominator)
+        {
+            Numerator = numerator;
+            Denominator = denominator;
+            Console.WriteLine($"2Constructor : \t{GetHashCode()}");
         }
 
-        private double FindGCD(double a, double b)
+        public Fraction(int integer = 0, int numerator = 0, int denominator = 1) : this(numerator, denominator)
         {
-            while (b != 0)
-            {
-                double temp = b;
-                b = a % b;
-                a = temp;
-            }
-            return a;
+            Integer = integer;
+            //Numerator = numerator;
+            //Denominator = denominator;
+            Console.WriteLine("Construtor:\t" + GetHashCode());
+        }
+        public Fraction(Fraction other)
+        {
+            if (this.Equals(other)) Console.WriteLine("You want to copy an object to itself");
+            this.Integer = other.Integer;
+            this.Numerator = other.Numerator;
+            this.Denominator = other.Denominator;
+            Console.WriteLine("CopyConstructor:\t" + GetHashCode());
+        }
+        ~Fraction()
+        {
+            Console.WriteLine("Destrutor:\t" + GetHashCode());
         }
 
+        //				Operators:
+        public static Fraction operator *(Fraction left_operand, Fraction right_operand)
+        {
+            Fraction left = new Fraction(left_operand);
+            Fraction right = new Fraction(right_operand);
+            left.ToImproper();
+            right.ToImproper();
+            return new Fraction(left.Numerator * right.Numerator, left.Denominator * right.Denominator).ToProper();
+        }
+        //---------------------------------------------------------------------------
+        public static bool operator ==(Fraction left_operand, Fraction right_operand)
+        {
+            Fraction left = new Fraction(left_operand);
+            Fraction right = new Fraction(right_operand);
+            left.ToImproper();
+            right.ToImproper();
+            return left.Numerator * right.Denominator == right.Numerator * left.Denominator;
+        }
+        public static bool operator !=(Fraction left, Fraction right)
+        {
+            return !(left == right);
+        }
+        //				 Methods:
+        public Fraction ToImproper()
+        {
+            Numerator += Integer * Denominator;
+            Integer = 0;
+            return this;
+        }
+        public Fraction ToProper()
+        {
+            Integer += Numerator / Denominator;
+            Numerator %= Denominator;
+            return this;
+        }
+        public void Print()
+        {
+            //Console.Write($"{Integer}({Numerator}/{Denominator})");
+            if (Integer != 0) Console.Write(Integer);
+            if (Numerator != 0)
+            {
+                if (Integer != 0) Console.Write("(");
+                Console.Write($"{Numerator}/{Denominator}");
+                if (Integer != 0) Console.Write(")");
+            }
+            else if (Integer == 0) Console.Write(0);
+            Console.WriteLine();
+        }
         public override string ToString()
         {
-            if (denominator == 1)
+            string fraction = "";
+            if (Integer != 0) fraction += Integer;
+            if (Numerator != 0)
             {
-                return numerator.ToString();
+                if (Integer != 0) fraction += "(";
+                fraction += $"{Numerator}/{Denominator}";
+                if (Integer != 0) fraction += ")";
             }
-            return $"{numerator}/{denominator}";
+            else if (Integer == 0) fraction = "0";
+            return fraction;
         }
 
-        public static Fraction operator +(Fraction fraction1, Fraction fraction2)
-        {
-            double resultNumerator = fraction1.numerator * fraction2.denominator + fraction2.numerator * fraction1.denominator;
-            double resultDenominator = fraction1.denominator * fraction2.denominator;
-            return new Fraction(resultNumerator, resultDenominator);
-        }
+#if CLASS_FRACTION
 
-        public static Fraction operator -(Fraction fraction1, Fraction fraction2)
-        {
-            double resultNumerator = fraction1.numerator * fraction2.denominator - fraction2.numerator * fraction1.denominator;
-            double resultDenominator = fraction1.denominator * fraction2.denominator;
-            return new Fraction(resultNumerator, resultDenominator);
-        }
+        //private double numerator;
+        //private double denominator;
 
-        public static Fraction operator *(Fraction fraction1, Fraction fraction2)
-        {
-            double resultNumerator = fraction1.numerator * fraction2.numerator;
-            double resultDenominator = fraction1.denominator * fraction2.denominator;
-            return new Fraction(resultNumerator, resultDenominator);
-        }
+        //public Fraction(double numerator, double denominator)
+        //{
+        //    if (denominator == 0)
+        //    {
+        //        throw new ArgumentException(" Zero cannot be Denominator.");
+        //    }
 
-        public static Fraction operator /(Fraction fraction1, Fraction fraction2)
-        {
-            if (fraction2.numerator == 0)
-            {
-                throw new DivideByZeroException("Division of zero not allow.");
-            }
+        //    this.numerator = numerator;
+        //    this.denominator = denominator;
+        //    Reduce();
+        //    Console.WriteLine($"2args Constructor :\t{GetHashCode()}");
+        //}
 
-            double resultNumerator = fraction1.numerator * fraction2.denominator;
-            double resultDenominator = fraction1.denominator * fraction2.numerator;
-            return new Fraction(resultNumerator, resultDenominator);
-        }
+        //private void Reduce()
+        //{
+        //    double gcd = FindGCD(Math.Abs(numerator), Math.Abs(denominator));
+        //    numerator /= gcd;
+        //    denominator /= gcd;
+
+        //    if (denominator < 0)
+        //    {
+        //        numerator = -numerator;
+        //        denominator = -denominator;
+        //    }
+        //}
+
+        //private double FindGCD(double a, double b)
+        //{
+        //    while (b != 0)
+        //    {
+        //        double temp = b;
+        //        b = a % b;
+        //        a = temp;
+        //    }
+        //    return a;
+        //}
+
+        //public override string ToString()
+        //{
+        //    if (denominator == 1)
+        //    {
+        //        return numerator.ToString();
+        //    }
+        //    return $"{numerator}/{denominator}";
+        //}
+
+        //public static Fraction operator +(Fraction fraction1, Fraction fraction2)
+        //{
+        //    double resultNumerator = fraction1.numerator * fraction2.denominator + fraction2.numerator * fraction1.denominator;
+        //    double resultDenominator = fraction1.denominator * fraction2.denominator;
+        //    return new Fraction(resultNumerator, resultDenominator);
+        //}
+
+        //public static Fraction operator -(Fraction fraction1, Fraction fraction2)
+        //{
+        //    double resultNumerator = fraction1.numerator * fraction2.denominator - fraction2.numerator * fraction1.denominator;
+        //    double resultDenominator = fraction1.denominator * fraction2.denominator;
+        //    return new Fraction(resultNumerator, resultDenominator);
+        //}
+
+        //public static Fraction operator *(Fraction fraction1, Fraction fraction2)
+        //{
+        //    double resultNumerator = fraction1.numerator * fraction2.numerator;
+        //    double resultDenominator = fraction1.denominator * fraction2.denominator;
+        //    return new Fraction(resultNumerator, resultDenominator);
+        //}
+
+        //public static Fraction operator /(Fraction fraction1, Fraction fraction2)
+        //{
+        //    if (fraction2.numerator == 0)
+        //    {
+        //        throw new DivideByZeroException("Division of zero not allow.");
+        //    }
+
+        //    double resultNumerator = fraction1.numerator * fraction2.denominator;
+        //    double resultDenominator = fraction1.denominator * fraction2.numerator;
+        //    return new Fraction(resultNumerator, resultDenominator);
+        //}  
+#endif
     }
 }
--- _Fraction/Program.cs
+++ _Fraction/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿//#define FIRST_FRACTION
+#define CONSTRUCTORS_CHECK
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,42 @@ namespace _Fraction
     {
         static void Main(string[] args)
         {
+
+#if CONSTRUCTORS_CHECK
+            Fraction A = new Fraction(); //Default constructor
+            A.Print();
+            Console.WriteLine(A);
+            Console.WriteLine(A.GetType());
+
+            Fraction B = new Fraction(5);
+            B.Print();
+            Console.WriteLine(B);
+
+            Fraction C = new Fraction(1, 2);
+            C.Print();
+            Console.WriteLine(C);
+
+            Fraction D = new Fraction(2, 3, 4);
+            D.Print();
+            Console.WriteLine(D);
+#endif
+            //Fraction A = new Fraction(2, 3, 4);
+            //Fraction B = new Fraction(3, 4, 5);
+            //Fraction C = new Fraction(A * B);
+
+            //Console.WriteLine(A);
+            //Console.WriteLine(B);
+            //Console.WriteLine(C);
+
+            ////Fraction D = new Fraction(A);
+            //////Fraction D = A;
+            ////Console.WriteLine(A.Equals(D));
+            ////A = new Fraction(A);
+
+            //Console.WriteLine(new Fraction(1, 2) != new Fraction(5, 10));
+
+
+#if FIRST_FRACTION
             //Fraction f1 = new Fraction(4, 5);
             //Fraction f2 = new Fraction(3, 7);
             //Console.WriteLine(f1.subtraction(f2));
@@ -33,6 +71,7 @@ namespace _Fraction
             Console.WriteLine($"Subtraction: {subtraction}");
             Console.WriteLine($"Multiply: {multiply}");
             Console.WriteLine($"Division: {division}");
+#endif 
 
         }
     }
